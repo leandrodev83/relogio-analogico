a0be27acddfb7343ce05dc58f3ac137ccc8f446c
Leandro Carvalho <le.carvalho.system83@gmail.com>
2022-01-26 15:05:19 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let digitalElement = document.querySelector(".digital");
+// let sElement = document.querySelector(".p_s");
+// let mElement = document.querySelector(".p_m");
+// let hElement = document.querySelector(".p_h");
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let secund = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(secund)}`;
+}
+
+function fixZero(time) {
+    if(time < 10) {
+        return "0"+time;
+    } else {
+        return time;
+    }
+}
+// return time < 10 ? `0${time}` : time; 
+setInterval(updateClock, 1000);
