1d36c2b14c27571791d3b8ebfea70a5fd7b45738
Leandro Carvalho <le.carvalho.system83@gmail.com>
2022-01-26 18:14:00 -0300

    Criando arquivo index.html e script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let digitalElement = document.querySelector(".digital");
-// let sElement = document.querySelector(".p_s");
-// let mElement = document.querySelector(".p_m");
-// let hElement = document.querySelector(".p_h");
+let sElement = document.querySelector(".p_s");
+let mElement = document.querySelector(".p_m");
+let hElement = document.querySelector(".p_h");
 
 function updateClock() {
     let now = new Date();
@@ -9,15 +8,14 @@ function updateClock() {
     let minute = now.getMinutes();
     let secund = now.getSeconds();
 
-    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(secund)}`;
+    let sDeg = ((360 / 60) * secund) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
-function fixZero(time) {
-    if(time < 10) {
-        return "0"+time;
-    } else {
-        return time;
-    }
-}
-// return time < 10 ? `0${time}` : time; 
 setInterval(updateClock, 1000);
+updateClock();
